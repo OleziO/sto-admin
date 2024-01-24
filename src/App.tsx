3d92a4d34435f8a6d0a3189ce7b068b233d2e9be
unknown <olezio.olezio@gmail.com>
2024-01-24 11:22:07 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import { SignIn } from "./pages/signin";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SignIn />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/signin/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+export const SignIn = () => {
+  return <div className={styles.signinContainer}>Test</div>;
+};
